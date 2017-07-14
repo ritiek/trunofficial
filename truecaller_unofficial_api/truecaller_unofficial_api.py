8b4fea4a15a9f31c7fa52ac95b0867d8769569cb
ritiek <ritiekmalhotra123@gmail.com>
2017-07-14 14:38:11 +0530

    Update docs

--- truecaller_unofficial_api/truecaller_unofficial_api.py
+++ truecaller_unofficial_api/truecaller_unofficial_api.py
@@ -1,32 +1,3 @@
-"""
-Unofficial API to the Truecaller phone number search
-
-https://github.com/ritiek/truecaller-unofficial-api
-
-MIT License
-
-Copyright (c) 2017 Ritiek Malhotra
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
-
 import urllib
 import json
 
@@ -58,8 +29,11 @@ class search:
         phone_parsed = parsed['data'][0]['phones'][0]
         address_parsed = parsed['data'][0]['addresses'][0]
 
+        """ Truecaller id """
         self.id = basic['id']
+        """ Truecaller name """
         self.name = basic['name']
+        """ Truecaller score """
         self.score = basic['score']
         self.access = basic['access']
         self.enhanced = basic['enhanced']
