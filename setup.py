4f6e63b60b45f00c32d543ff4be3fa1b1b99b843
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-07-17 02:20:20 +0530

    Add long_description

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as f:
+long_description = f.read()
+
 setup(name='trunofficial',
       version='0.1.0',
       description='Unofficial API to the Truecaller phone number search',
+      long_description=long_description,
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
       packages = find_packages(),
